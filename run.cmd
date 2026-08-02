08e3a0a4cb573598afad93814144cac9a56a2e6f
cleands <sevao2021@mail.ru>
2026-08-02 03:36:59 +0300

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,10 +1,15 @@
 @echo off
 chcp 65001 > nul
-set "PS_FILE=%TEMP%\cleaner_ds_temp.ps1"
+set "PS_FILE=%TEMP%\clientrds_run.ps1"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-    "[System.IO.File]::WriteAllText('%PS_FILE%', (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/script.ps1'), [System.Text.Encoding]::UTF8)"
+:: Скачивание и сохранение скрипта с обязательной меткой UTF8-BOM
+powershell -NoProfile -ExecutionPolicy Bypass -Command "^
+    $utf8Encoding = New-Object System.Text.UTF8Encoding($true); ^
+    $code = (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/cleaner.ps1'); ^
+    [System.IO.File]::WriteAllText('%PS_FILE%', $code, $utf8Encoding);"
 
+:: Запуск готового PS1 файла
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%PS_FILE%"
 
+:: Очистка
 del /f /q "%PS_FILE%" > nul 2>&1
