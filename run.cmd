f5e948d336b906599cd03230fc1d3b702ed75fc8
cleands <sevao2021@mail.ru>
2026-08-02 03:41:33 +0300

    Update script URL to use raw GitHub content

--- run.cmd
+++ run.cmd
@@ -3,7 +3,7 @@ chcp 65001 > nul
 set "PS_FILE=%TEMP%\cleanerds_run.ps1"
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "^
-    $code = (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/script.ps1'); ^
+    $code = (Invoke-RestMethod -Uri 'http://raw.githubusercontent.com/cleands/cds/refs/heads/main/script.ps1'); ^
     [System.IO.File]::WriteAllText('%PS_FILE%', $code, [System.Text.Encoding]::ASCII);"
 
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%PS_FILE%"
