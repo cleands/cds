502d20c7ac857404cc37850eb2b63940810c0cdd
cleands <sevao2021@mail.ru>
2026-08-02 04:39:27 +0300

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,18 +1,23 @@
 @echo off
 chcp 65001 >nul
 setlocal
-set "PS_FILE=%TEMP%\CleanerDS.ps1"
 
-powershell -NoProfile -ExecutionPolicy Bypass ^
-  -Command "try { Invoke-WebRequest 'http://raw.githubusercontent.com/cleands/cds/refs/heads/main/script.ps1' -OutFile '%PS_FILE%' -UseBasicParsing } catch { exit 1 }"
+:: Настройка путей
+set "URL=https://cleands.github.io/cds/script.ps1"
+set "PS_FILE=%TEMP%\CleanerDS_App.ps1"
+
+echo [CleanerDS] Подключение к серверу и загрузка...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "try { Invoke-WebRequest '%URL%' -OutFile '%PS_FILE%' -UseBasicParsing } catch { exit 1 }"
 
 if not exist "%PS_FILE%" (
-    echo Failed to download CleanerDS.
+    echo [Ошибка] Не удалось загрузить скрипт. Проверь интернет или доступность GitHub.
     pause
     exit /b 1
 )
 
+:: Запуск в полностью скрытом режиме
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%PS_FILE%"
 
+:: Удаляем временный файл после закрытия интерфейса
 del /f /q "%PS_FILE%" >nul 2>&1
 exit
